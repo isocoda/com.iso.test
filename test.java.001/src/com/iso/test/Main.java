9c5f7821e3b23103911db19feaa335563fedee41
test <test@test.com>
2011-08-04 10:58:23 +0900

    first commit message. 最初のコミットメッセージです。

---
+++ test.java.001/src/com/iso/test/Main.java
@@ -0,0 +1,21 @@
+package com.iso.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Main main = new Main();
+		main.helloWorld();
+	}
+
+	public String helloWorld() {
+		// TODO 自動生成されたメソッド・スタブ
+		String str = "Hello World.";
+		return str;
+	}
+
+}
